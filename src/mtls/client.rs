cf9f6ebda9af8317420a0c1679d43a323bace00c
Tom Stoepker <tom.stoepker@gmail.com>
2021-12-06 23:38:29 +0100

    task: do not panic in certificate constructor

--- src/mtls/client.rs
+++ src/mtls/client.rs
@@ -1,53 +1,12 @@
 mod builder;
 
-use std::{fs::File, io};
+use std::{error::Error, fmt, fs::File, io};
 
 pub use builder::Builder;
 use hyper::client::HttpConnector;
 use hyper_rustls::HttpsConnector;
 use rustls_pemfile::Item;
 use url::{ParseError, Url};
-
-/// Certificate represents a x509 certificate and it's
-/// corresponding private key used to sign client requests
-#[derive(Debug, Clone)]
-pub struct Certificate {
-    key: rustls::PrivateKey,
-    chain: Vec<rustls::Certificate>,
-}
-
-impl Certificate {
-    /// load_from_x509_key_pair reads and parses a public/private key pair from a pair
-    /// of files. The files must contain PEM encoded data.
-    pub fn load_from_x509_key_pair(
-        cert_file: &str,
-        key_file: &str,
-    ) -> Result<Certificate, Box<dyn std::error::Error>> {
-        let chain = {
-            let f = File::open(cert_file)?;
-            let mut f = io::BufReader::new(f);
-            rustls_pemfile::certs(&mut f)?
-                .iter()
-                .map(|der| rustls::Certificate(der.to_vec()))
-                .collect()
-        };
-
-        let key = {
-            let f = File::open(key_file).unwrap();
-            let mut f = io::BufReader::new(f);
-            match rustls_pemfile::read_one(&mut f)
-                .transpose()
-                .unwrap()
-                .unwrap()
-            {
-                Item::PKCS8Key(key) => rustls::PrivateKey(key),
-                _ => panic!("no private key found"),
-            }
-        };
-
-        Ok(Certificate { chain, key })
-    }
-}
 
 /// Client is used to make http requests
 pub struct Client {
@@ -66,5 +25,68 @@ impl Client {
             Some(url) => url.join(path),
             None => url::Url::parse(path),
         }
+    }
+}
+
+// TODO: define a better error
+#[derive(Debug)]
+struct PrivateKeyError {
+    message: String,
+}
+
+impl fmt::Display for PrivateKeyError {
+    fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
+        write!(f, "{}", self.message)
+    }
+}
+
+impl Error for PrivateKeyError {
+    fn description(&self) -> &str {
+        &self.message
+    }
+}
+
+/// Certificate represents a x509 certificate and it's
+/// corresponding private key used to sign client requests
+#[derive(Debug, Clone)]
+pub struct Certificate {
+    key: rustls::PrivateKey,
+    chain: Vec<rustls::Certificate>,
+}
+
+impl Certificate {
+    /// load_from_x509_key_pair reads and parses a public/private key pair from a pair
+    /// of files. The files must contain PEM encoded data.
+    pub fn load_from_x509_key_pair(
+        cert_file: &str,
+        key_file: &str,
+    ) -> Result<Certificate, Box<dyn Error>> {
+        let chain = {
+            let f = File::open(cert_file)?;
+            let mut f = io::BufReader::new(f);
+            rustls_pemfile::certs(&mut f)?
+                .iter()
+                .map(|der| rustls::Certificate(der.to_vec()))
+                .collect()
+        };
+
+        let key = {
+            let f = File::open(key_file).unwrap();
+            let mut f = io::BufReader::new(f);
+            match rustls_pemfile::read_one(&mut f)
+                .transpose()
+                .unwrap()
+                .unwrap()
+            {
+                Item::PKCS8Key(key) => rustls::PrivateKey(key),
+                _ => {
+                    return Err(Box::new(PrivateKeyError {
+                        message: "no private key found".to_string(),
+                    }))
+                }
+            }
+        };
+
+        Ok(Certificate { chain, key })
     }
 }
